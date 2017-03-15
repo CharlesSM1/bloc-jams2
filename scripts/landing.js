c79552c458c702edf46fbd75da30697d5bd39a3c
CharlesSM1 <fictitiouschampion@gmail.com>
2017-03-15 14:44:11 -0600

    Checkpoint 10 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,27 @@
-var animatePoints = function() {
- 
-     var points = document.getElementsByClassName('point');
- 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
- 
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+    point.style.opactiy = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scale(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
  };
+
+window.onload = function() {
+    if window.innerHeight > 950) {
+       animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addeventListener("scroll", function(event) {
+        console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
+        if(document.documentElementscrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
